f36f4eb9d0b25533fba44b125d04ebeb4f6ea9a3
Neel Wankhade <neelwankhade007@gmail.com>
2026-03-30 01:16:25 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.74%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,14 +1,7 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        s1 = list(s1)
-        s2 = list(s2)
-
-        for i in range(len(s1)):
-            for j in range(i+2, len(s2)):
-                if s1[i] != s2[i] and j-i == 2:
-                    s1[i], s1[j] = s1[j], s1[i]
-        
-        s1 = ''.join(s1)
-        s2 = ''.join(s2)
-        return s1 == s2
-        
+        a = sorted([s1[0], s1[2]])
+        b = sorted([s1[1], s1[3]])
+        c = sorted([s2[0], s2[2]])
+        d = sorted([s2[1], s2[3]])
+        return a==c and b==d
