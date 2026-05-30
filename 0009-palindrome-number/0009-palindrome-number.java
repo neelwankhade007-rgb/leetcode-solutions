3045ef9b5507ce252c6846e15f26d73b5850c42c
Neel Wankhade <neelwankhade007@gmail.com>
2026-05-31 01:53:04 +0530

    Time: 4 ms (100%), Space: 46.1 MB (33.61%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        while(x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == reversed || x == reversed / 10);
+    }
+}
