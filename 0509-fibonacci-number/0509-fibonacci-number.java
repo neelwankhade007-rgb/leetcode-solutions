dcf52d8eb914a207601a3139effd0cbe487c2b43
Neel Wankhade <neelwankhade007@gmail.com>
2026-05-31 02:44:22 +0530

    Time: 10 ms (25.45%), Space: 42.1 MB (32.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int fib(int n) {
+    int []dp;
+
+    public int solve(int n) {
         if (n <= 1) return n;
+        dp[n] = solve(n-1) + solve(n-2);
+        return dp[n];
+    }
 
-        int p2 = 0;  // fib(0) 
-        int p1 = 1;  // fib(1)
-
-        for (int i = 2; i <= n; i++) {
-            int curr = p1 + p2;
-            p2 = p1;
-            p1 = curr;
-        }
-
-        return p1;
+    public int fib(int n) {
+        dp = new int[n + 1];
+        return solve(n);
     }
 }
